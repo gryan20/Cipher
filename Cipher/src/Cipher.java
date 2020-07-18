ac72d8db2919bd616ffea72dd36473797c9d2a4b
gryan20 <67970791+gryan20@users.noreply.github.com>
2020-07-17 19:21:20 -0500

    update version
    Updated

--- Cipher/src/Cipher.java
+++ Cipher/src/Cipher.java
@@ -1,8 +1,3 @@
-//Gabby Ryan
-//Programming Fundamentals Summer Session 2
-//Cipher
-
-
 
 import java.util.Scanner;
 import java.util.Random;
@@ -10,62 +5,65 @@ import java.util.Random;
 
 public class Cipher {
 
+	public static void validateNumber(int numberToValidate) {
+		if (numberToValidate>19)
+		{ 
+			System.out.println("Please read directions and try again");
+			System.exit(0);
+		}
+	
+	}
+
 	public static void main(String[] args) {
-		Scanner scan = new Scanner (System.in);
+		Scanner scan = new Scanner(System.in);
 		Random rand = new Random();
-		
-		String enternumbers;
+
 		int total;
-		int firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber, encryptedNumber;
-		
+		int firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber;
 		int num1;
 		int num2;
 		int num3;
 		int num4;
-		int key = rand.nextInt(10); //I chose 10 to be inclusive to 9
-		
-		System.out.println("Please enter 5 numbers between 0 and 19");
-		System.out.println("First Number:");
-		firstNumber = scan.nextInt(); 
-		System.out.println("Second Number:");
-		secondNumber = scan.nextInt();
-		System.out.println("Third Number:");
-		thirdNumber = scan.nextInt();
-		System.out.println("Fourth Number:");
-		fourthNumber = scan.nextInt();
-		System.out.println("Fifth Number:");
-		fifthNumber = scan.nextInt();
-		
-		System.out.println("Your key is:" + key);
-		
-		
-		if (firstNumber > 19) //is there a way to do this for all of them together instead of separately?
-		{ //is it best system practice to have this be blank before and after the grouping?
-			System.out.println("Please read directions ad try again!");
-			System.exit(0);
-		}
-		 
-	
-		total = firstNumber + secondNumber + thirdNumber + fourthNumber + fifthNumber;
-		
-		//Split number to digits 0 through 9
-		
-		while (total>0)
-		{
-			num1 = total/10;
-			num2 = total%10; //I am not sure if I did this right...
-		}
-		num1 = total/10;
-		num2 = total%10;
-		num3 = (num1 + key)%10; //what does it mean when num1 hasn't been initialized? I don't want to assign a number above so should I use string
-		num4 = (num2 + key)%10;
-		
-		encryptedNumber = num3 + num4;
-		
-		System.out.println(total); // might have to delete this line later
-		
-		System.out.println("Encrypted Number is:" + encryptedNumber);
-		
-	}
+		int key = rand.nextInt(10); // I chose 10 to be inclusive to 9
 
+		System.out.println("Programming Fundamentals");
+		System.out.println("NAME: Gabby");
+		System.out.println("PROGRAMMING ASSIGNMENT 1 - Cipher");
+		System.out.println("");
+		System.out.println("Welcome to the Cipher Program");
+		System.out.println("Please enter 5 numbers between 0 and 19");
+		System.out.println("1st Number: ");
+		firstNumber = scan.nextInt();
+		validateNumber(firstNumber);
+		System.out.println("2nd Number: ");
+		secondNumber = scan.nextInt();
+		validateNumber(secondNumber);
+		System.out.println("3rd Number: ");
+		thirdNumber = scan.nextInt();
+		validateNumber(thirdNumber);
+		System.out.println("4th Number: ");
+		fourthNumber = scan.nextInt();
+		validateNumber(fourthNumber);
+		System.out.println("5th Number: ");
+		fifthNumber = scan.nextInt();
+		validateNumber(fifthNumber);
+
+		scan.close();
+
+		total = firstNumber + secondNumber + thirdNumber + fourthNumber + fifthNumber;
+
+		//Here is where I split the total number to digits 0 through 9 for the tens and ones place
+
+		num1 = (total / 10); // for the tens place
+		num2 = total % 10; // for the ones place
+		num3 = ((num1 + key) % 10);
+		String eTenDigit = "" + num3; //e is for encrypted
+		num4 = ((num2 + key) % 10);
+		String eOneDigit = "" + num4;
+
+		System.out.println("Total = " + total);
+		System.out.println("Your random key is: " + key);
+		System.out.println("Encrypted Number: " + eTenDigit + eOneDigit);
+
+	}
 }
